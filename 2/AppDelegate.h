fb8cfc13441f9fdef18eb43f4091e7061d3f1af7
Pavel Aksenkin <pa@monosnap.com>
2013-03-18 23:46:20 -0700

    Начальное закрепление

---
+++ 2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  2
+//
+//  Created by Pavel Aksenkin on 18.03.13.
+//  Copyright (c) 2013 Pavel Aksenkin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
